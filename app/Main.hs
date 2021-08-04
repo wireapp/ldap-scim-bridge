b378c10421c1907f4eb9756d895b6d07e49656ea
Matthias Fischmann <mf@zerobuzz.net>
2021-08-04 21:55:59 +0200

    Fix copyright; ormolize.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,5 @@ module Main (main) where
 
 import LdapScimBridge (someFunc)
 
-
 main :: IO ()
 main = someFunc
--- src/LdapScimBridge.hs
+++ src/LdapScimBridge.hs
@@ -1,15 +1,7 @@
-{- |
-Copyright: (c) 2021 Matthias Fischmann
-SPDX-License-Identifier: AGPL-3.0-only
-Maintainer: Matthias Fischmann <mf@zerobuzz.net>
-
-See README for more info
--}
-
 module LdapScimBridge
-       ( someFunc
-       ) where
-
+  ( someFunc,
+  )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
