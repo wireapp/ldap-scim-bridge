bd21d9840326073fb43af0a2642539149ed91c24
Matthias Fischmann <mf@zerobuzz.net>
2021-10-26 12:31:39 +0200

    Remove redundant imports.

--- src/LdapScimBridge/Mapping.hs
+++ src/LdapScimBridge/Mapping.hs
@@ -3,37 +3,14 @@
 
 module LdapScimBridge.Mapping where
 
-import Control.Exception (ErrorCall (ErrorCall), catch, throwIO)
 import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.Encode.Pretty as Aeson
-import qualified Data.ByteString.Char8 as ByteString
-import qualified Data.Foldable as Foldable
-import qualified Data.HashMap.Lazy as HM
-import qualified Data.List
 import qualified Data.Map as Map
-import Data.String.Conversions (cs)
 import qualified Data.String.Conversions as SC
-import qualified Data.Text.Encoding as Text
-import qualified Data.Yaml as Yaml
 import qualified GHC.Show
-import Ldap.Client as Ldap
-import qualified Network.HTTP.Client as HTTP
-import qualified Network.HTTP.Client.TLS as HTTP
-import Servant.API.ContentTypes (NoContent)
-import Servant.Client (BaseUrl (..), ClientEnv (..), Scheme (..), mkClientEnv)
-import System.Environment (getProgName)
-import System.Logger (Level (..))
-import qualified System.Logger as Log
 import qualified Text.Email.Validate
 import Web.Scim.Class.Auth (AuthData)
 import qualified Web.Scim.Class.Auth as AuthClass
 import qualified Web.Scim.Class.Group as GroupClass
-import qualified Web.Scim.Class.User as ScimClass
-import qualified Web.Scim.Client as ScimClient
-import qualified Web.Scim.Filter as ScimFilter
-import qualified Web.Scim.Schema.Common as ScimCommon
-import qualified Web.Scim.Schema.ListResponse as Scim
-import qualified Web.Scim.Schema.Meta as Scim
 import qualified Web.Scim.Schema.Schema as Scim
 import qualified Web.Scim.Schema.User as Scim
 import qualified Web.Scim.Schema.User.Email as Scim
