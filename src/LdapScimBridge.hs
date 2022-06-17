d68e33a0a1097525fbc1544d51bf491ea89c859f
fisx <mf@zerobuzz.net>
2022-06-17 11:21:07 +0200

    Update src/LdapScimBridge.hs
    Co-authored-by: Sven Tennie <sven.tennie@gmail.com>

--- src/LdapScimBridge.hs
+++ src/LdapScimBridge.hs
@@ -186,7 +186,7 @@ data FieldMapping = FieldMapping
 instance Show FieldMapping where
   show = show . fieldMappingLabel
 
--- Fill in the parameters for hscim 'User' type with plausible defaults.  (You may want to
+-- | Fill in the parameters for hscim 'User' type with plausible defaults.  (You may want to
 -- touch this if you're using the library for something new.)
 data ScimTag
 
