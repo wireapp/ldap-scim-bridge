6d4dfeb3f1d7b8dc798482c7c1eb8cb8d26b9f87
Leif Battermann <leif.battermann@wire.com>
2024-03-12 15:23:31 +0000

    add formatters/tools to env

--- shell.nix
+++ shell.nix
@@ -3,8 +3,9 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/7eeacecff44e05a9fd61b9e03836b66ecde8a525.tar.gz";
     sha256 = "sha256:0f6nv0pgk58d1962r8vswi7ks59fryh0yrdk99d30b3qj11a2045";
   };
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
 in
+
 
 pkgs.mkShellNoCC rec {
   nativeBuildInputs = with pkgs; [
@@ -12,6 +13,10 @@ pkgs.mkShellNoCC rec {
     ghcid
     ghc
     zlib
+    (haskell.lib.justStaticExecutables pkgs.haskell.packages.ghc94.ormolu_0_5_2_0)
+    (haskell.lib.justStaticExecutables haskellPackages.cabal-fmt)
+    nixpkgs-fmt
+    treefmt
   ];
 
   # Ensure that libz.so and other libraries are available to TH
